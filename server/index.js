b8eb20ec935787a3eb1c1b569b888d8c563d3458
ShishirVolety <shishir.volety99@gmail.com>
2023-01-27 17:20:33 -0500

    MERN tutorial

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://svolety:Onepiece123@cluster0.ledw2xi.mongodb.net/merntutorial?retryWrites=true&w=majority"
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUsers", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("Server running");
+});
